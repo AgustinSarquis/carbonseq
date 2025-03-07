617c42fb427f1e0686da70c2701fe0c6c72faf66
Agustín Sarquis <agusarquis@agro.uba.ar>
2025-03-06 17:33:04 -1000

    vjklndflknflkd

---
+++ draft.R
@@ -0,0 +1,9 @@
+#################
+# CBS R package #
+#################
+
+# What do I want the package to do?
+
+library(devtools)
+packageVersion("devtools")
+create_package('~/Documents/toypackage')
